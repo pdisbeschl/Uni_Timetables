37b37d3dae466a00a3d80e25e5afe4667ba14b14
GuilleQP <36505071+GuilleQP@users.noreply.github.com>
2020-03-04 14:27:58 +0100

    :construction: Scheduler skeleton and sample files

---
+++ src/scheduler.py
@@ -0,0 +1,41 @@
+"""
+Timetable scheduler
+===================
+
+author: Huy Ngo
+author: Yu Fee Chan
+author: Daniel Kaestner 
+author: Paul Disbeschl 
+author: Guillermo Quintana Pelayo
+author: Camilla Lummerzheim 
+
+Documented following PEP 257.
+"""
+
+class Scheduler:
+    """Main scheduler class
+
+    Generates a timetable according to the 
+    specified input data and selected
+    algorithm.
+    """
+
+    def __init__(self):
+        pass
+
+    def generate_timetable(self, input_dir, algorithm):
+        """Generates a timetable.
+
+        Input
+        -----
+        input_dir: string
+            path where input files are located.
+        algorithm: type
+            algorithm to use to generate the timetable.
+        """
+        pass
+
+    def __evaluate(self):
+        """Evaluates a timetable.
+        """
+        pass
