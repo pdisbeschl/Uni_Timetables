b13507dc3819a018495388f45f04fbf5acdb5044
pdisbeschl <pdisbeschl@users.noreply.github.com>
2020-04-19 00:12:00 +0200

    :sparkles: Classes no longer overlap for weekly schedules :100:
    Electives are ignored.
    TODO: Implement this on a wider scale.

--- src/algorithms/weekly.py
+++ src/algorithms/weekly.py
@@ -54,8 +54,8 @@ class Weekly(Scheduler):
         self.generate_weekly_timetable(weekly_courses)
 
     """ 
-    dividing weeks of a PERIOD by 7, excludes the exam week, converts to int
-    Assumption: Block starts on a Monday, ends on a Friday
+    The following method divides the PERIOD by the number of weeks (usually 7), excludes the exam week, converts to int
+    Assumption: Block starts on a Monday, ends on a Friday (the dividing by 7 is for the 7 days in a week)
     """
     def week_course_splitter(self):
         weekly_courses = self.hard_constraints.get_courses()
@@ -79,27 +79,49 @@ class Weekly(Scheduler):
         #Iterate over all courses
         for course_id, course in weekly_courses.items():
             print("Processing " + course_id)
+            prog_id = course['Programme']
             #Try to put a course into a timeslot and subtract the given contact hourse. Repeat until the course has no contact hours left
             while course['Contact hours'] > 0:
                 contact_hours = course['Contact hours']
                 #Iterate over all timeslots
                 for timeslot in free_timeslots:
+                    #Check if the programme (year group) isn't already in another class
+                    if self.has_prog_conflict(weekly_courses, timeslot, course):
+                        continue
+
                     #Check, if the lecturer is already teaching a course at that time
                     if self.has_lecturer_conflict(weekly_courses, timeslot, course):
                         continue
+
 
                     #Look for a free room for the given timeslot
                     room_id = self.find_free_room(course, timeslot, rooms.copy())
                     if room_id == None:
                         continue
 
-                    self.schedule.setdefault(timeslot, []).append({"CourseID" : course_id, "RoomID" : room_id})
+                    self.schedule.setdefault(timeslot, []).append({"CourseID" : course_id, "ProgID" : prog_id, "RoomID" : room_id})
                     course['Contact hours'] -= 2
                     break
 
                 #If we could not place the course, throw an exception
                 if contact_hours == course['Contact hours']:
                     raise Exception('Cannot brute force a schedule for the given constraints')
+
+    """
+    The following method checks to see if there is no conflict between classes of the same programme (year-group).
+    Idea: Programmes can't be scheduled at the same time unless the classes are electives.
+    """
+    def has_prog_conflict(self, courses, timeslot, course):
+        if timeslot not in self.schedule.keys():
+            return False
+        #Iterate over all scheduled courses in a timeslot
+        for scheduled_course_info in self.schedule[timeslot]:
+            scheduled_course = courses[scheduled_course_info['CourseID']]
+            # Check if the students of the currently 'to-be-planned course' are already in a class (Excluding Electives!)
+            if scheduled_course['Programme'] == course['Programme']:
+                if course['Elective']==0:
+                    return True
+        return False
 
     def has_lecturer_conflict(self, courses, timeslot, course):
         if timeslot not in self.schedule.keys():
@@ -113,7 +135,6 @@ class Weekly(Scheduler):
                 if lecturer in course['Lecturers']:
                     return True
         return False
-
 
     def find_free_room(self, course, timeslot, rooms):
         if timeslot in self.schedule.keys():
