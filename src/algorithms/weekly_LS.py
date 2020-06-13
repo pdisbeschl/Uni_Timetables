14b5897a315877165b65376b562e8d7c7199811c
yufeechan <57565595+yufeechan@users.noreply.github.com>
2020-06-13 20:19:44 +0200

    Added local search function
    The local search should work now. It only needs to integrate the evaluation function within it.

--- src/algorithms/weekly_LS.py
+++ src/algorithms/weekly_LS.py
@@ -22,6 +22,8 @@ import json, os
 from datetime import timedelta
 import math
 import copy
+import numpy as np
+import random
 
 class Weekly_LS(Scheduler):
 #    logFile = open(os.path.realpath('./Logs/log.txt'), "a")
@@ -71,13 +73,12 @@ class Weekly_LS(Scheduler):
     
     """
     This is a very very hacky brute force algorithm to generate a simple feasible thing """
-    def create_greedy_chrom(self, events, room_time_avb, courses):
+    def create_greedy_chrom(self, events, room_time_avb, room_times, courses):
         chrom_greedy = {}
         
         # make a schedule in a greedy way
         for i,event in enumerate(events):
             print("Processing " + event["CourseID"])
-            prog_id = event["CourseInfo"]["Programme"]
             course = event["CourseInfo"]
             course_id = event["CourseID"]
             # try to put in first available room-time
@@ -93,12 +94,142 @@ class Weekly_LS(Scheduler):
                 
                 # remove room-time from the list
                 room_time_avb.pop(room_id+"_"+timeslot)
+                room_times[room_id].remove(timeslot)
                 
                 # update chromosome
                 chrom_greedy["Gen"+str(i)] = {"CourseID": course_id, "TimeSlot":timeslot, "RoomID":room_id}
                 break
-        return chrom_greedy
+        return chrom_greedy, room_times
+    
+        """
+    This is a very very hacky brute force algorithm to generate a simple feasible thing """
+    def create_random_chrom(self, events, room_time_avb, room_times, courses):
+        chrom = {}
+        
+        # make a schedule in a greedy way
+        for i,event in enumerate(events):
+            print("Processing " + event["CourseID"])
+            course = event["CourseInfo"]
+            course_id = event["CourseID"]
+            allocated = False
+            while not allocated:
+                # try to put in first available room-time
+                rt_key, room_time = random.choice(list(room_time_avb.items()))
+                timeslot = room_time["TimeSlot"]
+                room_id = room_time["RoomID"]
+                
+                #Check if the lecturer is already teaching a course at that time
+                # if clash, go to next room-time combo
+                if self.has_lecturer_conflict(chrom, courses, timeslot, course):
+                    continue
+                
+                # remove room-time from the list
+                allocated = True
+                room_time_avb.pop(room_id+"_"+timeslot)
+                room_times[room_id].remove(timeslot)
+                
+                # update chromosome
+                chrom["Gen"+str(i)] = {"CourseID": course_id, "TimeSlot":timeslot, "RoomID":room_id}
+                break
+        return chrom, room_times
+    
+    def chrom_to_schedule(self, chrom, timeslots, courses):
+        schedule = {}
+        for t in timeslots:
+            schedule[t] = []
+        # translate chromosome into schedule
+        for gene_no, gene in chrom.items():
+            timeslot = gene["TimeSlot"]
+            course_id = gene["CourseID"]
+            prog_id = courses[gene["CourseID"]]["Programme"]
+            room_id = gene["RoomID"]
+            schedule[timeslot].append({"CourseID" : course_id
+                                    , "ProgID" : prog_id, "RoomID" : room_id,
+                                    "Gene_no": gene_no})
+        return schedule
+    
+    def local_search(self, individual, num_iter = 100):
+        def worst_removal(individual,q=1):
+            p = 6
+            cost_before = individual['Score']
+            # copy of the schedule, such that it is LOCAL!!
+            schedule = copy.deepcopy(individual['Schedule'])
+            cost_diff = []
+            removed = []
+            while q > 0:
+                event_list = []
+                delta_costs = []
+                for timeslot, events in schedule.items():
+                    events_copy = copy.deepcopy(events)
+                    for i,event in enumerate(events_copy):
+                        event_copy = copy.deepcopy(event)
+                        events.remove(event_copy)
+                        delta_cost = np.random.uniform(0,1) ############
+                        delta_costs.append(delta_cost)
+                        events.append(event_copy)
+                        event_list.append((timeslot,event_copy))
+                
+                # find which event to remove
+                index_sorted = sorted(range(len(delta_costs)), key=lambda k: delta_costs[k])
+                y = np.random.uniform(0,1)
+                event_removed = event_list[index_sorted[math.floor(y**p*len(event_list))]]
+                removed.append(event_removed)
+                cost_diff.append(delta_costs[index_sorted[math.floor(y**p*len(event_list))]])
+                # remove from schedule copy
+                schedule[event_removed[0]].remove(event_removed[1])
+                q -= 1
+            # update individual
+            individual['Schedule'] = schedule
+            individual['Score'] = cost_before - sum(cost_diff) #######
+            # r[0] = timeslot, r[1] = event
+            for r in removed:
+                individual['Chromosome'][r[1]['Gene_no']] = None
+                individual['Availables'][r[1]['RoomID']].append(r[0])
+            return removed, individual
+        
+        def greedy_insertion(removed, individual):
+            schedule = copy.deepcopy(individual['Schedule'])
+            cost_before = individual['Score']
+            cost_diff = []
+            insert_locs = []
+            for i in range(len(removed)):
+                cost_diff.append([])
+                for room_id, timeslots in individual['Availables'].items():
+                    if len(timeslots) > 0:
+                        for timeslot in timeslots:
+                            if i == 0:
+                                insert_locs.append((room_id, timeslot))
+                            schedule[timeslot].append(removed[i][1])
+                            cost_diff[i].append(999 - cost_before) ################
+                            schedule[timeslot].remove(removed[i][1])
+            cost_diff = np.array(cost_diff)
+            min_costs = np.amin(cost_diff,axis=1)
+            event_index = np.argmin(min_costs)
+            event_insert = removed[event_index][1]
+            # new position of the before removed event [0]:room_id, [1]:timeslot
+            insert_loc = insert_locs[np.argmin(cost_diff[event_index,:])]
+            # change room of event
+            event_insert['RoomID'] = insert_loc[0]
+            # insert in schedule
+            schedule[insert_loc[1]].append(event_insert)
+            print(event_insert, insert_loc)
+            individual['Schedule'] = schedule
+            individual['Chromosome'][event_insert['Gene_no']] = ({'CourseID': event_insert['CourseID'],
+                      'RoomID': insert_loc[0], 'TimeSlot': insert_loc[1]})
+            individual['Availables'][insert_loc[0]].remove(insert_loc[1])
+            individual['Score'] += min_costs # cost update
+            removed.remove(removed[event_index])
+            return removed, individual
+        
+        for i in range(num_iter):
+            removed, individual_r = worst_removal(individual, 2)
+            while len(removed) > 0:
+                removed, individual_r = greedy_insertion(removed, individual_r)
             
+        return individual_r
+            
+        
+        
     
     def generate_weekly_timetable(self,weekly_courses):
         #The final json shedule in the format: {BAY1: {Timeslot: CouseID, Timeslot:CourseID}, BAY2:{...}...}
@@ -107,17 +238,19 @@ class Weekly_LS(Scheduler):
         
         # create events for each course: each event is max 2 contact hours
         # events with 1 hour duration still take full timeslot
-        events = []
+        all_events = []
         programmes = []
         for course_id, course in weekly_courses.items():
             if course["Programme"] not in programmes:
                 programmes.append(course["Programme"])
             for i in range(math.ceil(course["Contact hours"]/2)):
-                events.append({"CourseID": course_id, "CourseInfo": course})
+                all_events.append({"CourseID": course_id, "CourseInfo": course})
                 
         
         # create dictionary of all timeslots and rooms combinations
         timeslots = copy.deepcopy(self.hard_constraints.get_free_timeslots())[0:20]
+        timeslots_str = [str(t) for t in timeslots]
+        timeslots = timeslots_str
         times = []
         days = []
         for t in timeslots:
@@ -126,8 +259,10 @@ class Weekly_LS(Scheduler):
             if t[-8:] not in times:
                 times.append(t[-8:])
         rooms = self.hard_constraints.get_rooms()
+        rooms_times = {}
         rt_avb = {}
         for room_id, room_info in rooms.items():
+            rooms_times[room_id] = copy.deepcopy(timeslots)
             for t in timeslots:
                 rt_avb[room_id+"_"+t] = {"RoomID":room_id,"TimeSlot":t}
         room_time_avb = copy.deepcopy(rt_avb)
@@ -137,22 +272,31 @@ class Weekly_LS(Scheduler):
         population = []        
         # a chromosome is a dictionary with genes, where each gene is a dictionary
         # with {courseID, timeslot, roomID}
-        chrom_greedy = self.create_greedy_chrom(events, room_time_avb, weekly_courses)
-        population.append(chrom_greedy)    
+        chrom,avb = self.create_greedy_chrom(all_events, copy.deepcopy(room_time_avb),
+                                                    copy.deepcopy(rooms_times), weekly_courses)
+        sched = self.chrom_to_schedule(chrom, timeslots, weekly_courses)
+        population.append({'Chromosome':chrom, 'Availables': avb, 
+                           'Score':float('nan'), 'Schedule': sched})
+        for i in range(39):
+            chrom, avb = self.create_random_chrom(all_events, copy.deepcopy(room_time_avb),
+                                                    copy.deepcopy(rooms_times), weekly_courses)
+            sched = self.chrom_to_schedule(chrom, timeslots, weekly_courses)
+            population.append({'Chromosome':chrom, 'Availables': avb, 
+                           'Score':float('nan'), 'Schedule': sched})
         
         
         
-        chrom_final = chrom_greedy 
-        # translate chromosome into schedule
-        for gene_no, gene in chrom_final.items():
-            timeslot = gene["TimeSlot"]
-            course_id = gene["CourseID"]
-            prog_id = weekly_courses[gene["CourseID"]]["Programme"]
-            room_id = gene["RoomID"]
-            self.schedule.setdefault(timeslot, []).append({"CourseID" : course_id+
-                "("+str(weekly_courses[course_id]['Elective'])+")", "ProgID" : prog_id, "RoomID" : room_id})
+#        chrom_final = chrom_greedy 
+#        # translate chromosome into schedule
+#        for gene_no, gene in chrom_final.items():
+#            timeslot = gene["TimeSlot"]
+#            course_id = gene["CourseID"]
+#            prog_id = weekly_courses[gene["CourseID"]]["Programme"]
+#            room_id = gene["RoomID"]
+#            self.schedule.setdefault(timeslot, []).append({"CourseID" : course_id+
+#                "("+str(weekly_courses[course_id]['Elective'])+")", "ProgID" : prog_id, "RoomID" : room_id})
 
-        
+
     """
     Creates a schedule per program: per day and per timeslot will be either empty
     or a scheduled course
