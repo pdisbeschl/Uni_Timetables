0f16a2e3fdaa63243303c4578aa02bb00f2980b9
Huy <ngo.huy.167@gmail.com>
2020-06-16 00:52:38 +0200

    update hard constraints, add soft constraints

--- src/algorithms/genetic.py
+++ src/algorithms/genetic.py
@@ -29,6 +29,7 @@ idea:
 
 """
 
+
 class Genetic(Scheduler):
     def __init__(self):
         super().__init__()
@@ -37,29 +38,33 @@ class Genetic(Scheduler):
         self.individual_size = 20 * 8
         self.generations = 2000
         self.crossover_probability = 0.8
-        self.mutation_probability = 0.05
+        self.mutation_probability = 0.01
         self.rooms = self.hard_constraints.get_rooms()
-        self.courses = self.hard_constraints.get_courses()
+        self.default_courses = self.hard_constraints.get_courses()
         self.timeslots = self.hard_constraints.get_free_timeslots()
         self.holidays = self.hard_constraints.get_holidays()
-        # self.courses = self.divide_courses_to_slot(self.hard_constraints.get_courses())
+        self.courses = self.divide_courses_to_slot()
 
     def generate_timetable(self):
         self.create_population()
         pop = self.population
         final_idx = 0
         count = 1
+        check_soft_constraints = True
 
-        # no divide =21; divide =58
-        # num of conflict * course
-        max_fit = 5 * 21
+        # num of constraints * courses
+        if check_soft_constraints:
+            max_fit = 6 * len(self.courses) + 1
+            self.generations *= 2
+        else:
+            max_fit = 5 * len(self.courses)
+
         # stop when satisfying all hard constraints or reach generation th
         while self.fitness(pop[final_idx]) < max_fit and count < self.generations:
             list_fitness_pop = []
             for i in pop:
-                score = self.fitness(i)
+                score = self.fitness(i, check_soft_constraints)
                 list_fitness_pop.append(score)
-
             # TODO: test only remove for when done
             for i in range(1):
                 parent1, parent2 = self.selection()
@@ -80,14 +85,18 @@ class Genetic(Scheduler):
             final_idx = np.argmax(list_fitness_pop)
 
             count += 1
-            print("gen {} best fit: {} rate {:.2f}%".format(
-                count, self.fitness(pop[final_idx]), self.fitness(pop[final_idx]) / max_fit * 100))
+            if self.fitness(pop[final_idx]) >= 5 * len(self.courses):
+                print("gen {} best fit: {}/{} rate {:.2f}% (maybe satisfy all hard constraints)".format(
+                    count, self.fitness(pop[final_idx]), max_fit, self.fitness(pop[final_idx]) / max_fit * 100))
+            else:
+                print("gen {} best fit: {}/{} rate {:.2f}%".format(
+                    count, self.fitness(pop[final_idx]), max_fit, self.fitness(pop[final_idx]) / max_fit * 100))
         print("done!")
         print("fail {}".format(max_fit - self.fitness(pop[final_idx])))
         result = pop[final_idx]
         result = self.fill_timeslot(result)
         result = self.convert_to_schedule(result)
-        self.copy_schedule(result, distribution_timeslot=False)
+        self.copy_schedule(result, distribution_timeslot=True)
 
     def crossover(self, individual1, individual2):
         individual1_new = individual1.copy()
@@ -172,14 +181,15 @@ class Genetic(Scheduler):
         parent2 = population[par2_idx]
         return parent1, parent2
 
-    def fitness(self, individual):
+    def fitness(self, individual, check_soft_constraints=False):
         courses = self.courses
         rooms = self.rooms
         list_courses = list(courses)
         lis_rooms = list(rooms)
-        count = 0
         score = 0
-        for g in individual:
+        day_off_score = {}
+
+        for count, g in enumerate(individual):
             if g != 0:
                 c = courses[list_courses[int(g) - 1]]
                 r = rooms[lis_rooms[count % 8]]
@@ -198,10 +208,40 @@ class Genetic(Scheduler):
                     score += 1
                 if not self.has_programme_conflict(g, count, individual):
                     score += 1
-                # start at 11pm (soft constraint)
-                # if s == 2:
-                #     score += 1
-            count += 1
+                # use to satisfy soft constraints
+                if check_soft_constraints:
+                    # start at 11pm
+                    if s == 2:
+                        score += 1
+                    # Friday FREEDOM !!!
+                    if count < 32:
+                        if "Mon" not in day_off_score.keys():
+                            day_off_score["Mon"] = 1
+                        else:
+                            day_off_score["Mon"] += 1
+                    elif 32 <= count < 64:
+                        if "Tue" not in day_off_score.keys():
+                            day_off_score["Tue"] = 1
+                        else:
+                            day_off_score["Tue"] += 1
+                    elif 64 <= count < 96:
+                        if "Wed" not in day_off_score.keys():
+                            day_off_score["Wed"] = 1
+                        else:
+                            day_off_score["Wed"] += 1
+                    elif 96 <= count < 128:
+                        if "Thu" not in day_off_score.keys():
+                            day_off_score["Thu"] = 1
+                        else:
+                            day_off_score["Thu"] += 1
+                    else:
+                        if "Fri" not in day_off_score.keys():
+                            day_off_score["Fri"] = 1
+                        else:
+                            day_off_score["Fri"] += 1
+
+        if check_soft_constraints and min(day_off_score, key=day_off_score.get) == "Fri":
+            score += 1
         return score
 
     # individual is list of classes (course with timeslot and room)
@@ -232,7 +272,8 @@ class Genetic(Scheduler):
             for i in data:
                 if i != 0:
                     course_id = list_courses[int(i) - 1]
-                    course = self.courses[course_id]
+                    course_id = course_id.split("_")[0]
+                    course = self.default_courses[course_id]
                     prog_id = course["Programme"]
                     room_id = list_rooms[c_room]
                     self.schedule.setdefault(str(self.timeslots[count]), []).append(
@@ -250,7 +291,7 @@ class Genetic(Scheduler):
         period_end = self.hard_constraints.period_info["EndDate"]
         week_counter = 1
         first_lecture = next(iter(schedule.keys()))
-        courses = self.courses
+        courses = self.default_courses
         while first_lecture + timedelta(days=7 * week_counter) < period_end:
             list_slots_per_week = {}
             for date, scheduled_courses in schedule.items():
@@ -294,7 +335,6 @@ class Genetic(Scheduler):
         gen_course_id = list_courses[int(gen) - 1]
         gen_course = courses[gen_course_id]
         lecturers = gen_course["Lecturers"].split(';')
-        contact_hours = int(gen_course["Contact hours"] / 2)
         needed_slot_per_week = self.calculate_need_slot_per_week(gen_course_id)
 
         check_slot = low_slot + (needed_slot_per_week * 8)
@@ -320,7 +360,6 @@ class Genetic(Scheduler):
         gen_course_id = list_courses[int(gen) - 1]
         gen_course = courses[gen_course_id]
 
-        contact_hours = int(gen_course["Contact hours"] / 2)
         needed_slot_per_week = self.calculate_need_slot_per_week(gen_course_id)
         if (pos + 8 * (needed_slot_per_week - 1)) > end_slot:
             return True
@@ -338,7 +377,6 @@ class Genetic(Scheduler):
         gen_course_id = list_courses[int(gen) - 1]
         gen_course = courses[gen_course_id]
 
-        contact_hours = int(gen_course["Contact hours"] / 2)
         needed_slot_per_week = self.calculate_need_slot_per_week(gen_course_id)
         for i in range(pos + 8, end_slot + 1, 8):
             needed_slot_per_week -= 1
@@ -356,7 +394,6 @@ class Genetic(Scheduler):
         gen_course_id = list_courses[int(gen) - 1]
         gen_course = courses[gen_course_id]
         programme = gen_course["Programme"]
-        contact_hours = int(gen_course["Contact hours"] / 2)
         needed_slot_per_week = self.calculate_need_slot_per_week(gen_course_id)
 
         check_slot = low_slot + (needed_slot_per_week * 8)
@@ -379,7 +416,6 @@ class Genetic(Scheduler):
                 end_slot = start_slot + 24
                 gen_course_id = list_courses[int(value) - 1]
                 gen_course = courses[gen_course_id]
-                contact_hours = int(gen_course["Contact hours"] / 2)
                 needed_slot_per_week = self.calculate_need_slot_per_week(gen_course_id)
                 needed_slot_per_week -= 1
                 for i in range(idx + 8, end_slot + 1, 8):
@@ -392,40 +428,37 @@ class Genetic(Scheduler):
         return new_timetable
 
     def calculate_need_slot_per_week(self, course_id):
-        list_courses_per_programme = {}
-        for v in self.courses.values():
-            programme_name = v["Programme"]
-            if programme_name not in list_courses_per_programme.keys():
-                list_courses_per_programme[programme_name] = 1
-            else:
-                list_courses_per_programme[programme_name] += 1
-
-        course_programme = self.courses[course_id]["Programme"]
         contact_hours = int(self.courses[course_id]["Contact hours"] / 2)
-        if list_courses_per_programme[course_programme] <= 5:
-            needed_slot_per_week = math.ceil(contact_hours / 7)
-        else:
-            if contact_hours <= 16:
-                needed_slot_per_week = math.ceil(contact_hours / 7)
-            else:
-                needed_slot_per_week = math.floor(contact_hours / 7)
+        needed_slot_per_week = math.ceil(contact_hours / 7)
         return needed_slot_per_week
 
-    # course with 36h or 34h occupies 3 slots/week
-    # course with 20h or 22h occupies 2 slots/w
-    # course with 12h occupies 1 slot/w
-    # total 58/160 slots/week
-    # TODO: currently not use
-    def divide_courses_to_slot(self, courses):
+    def divide_courses_to_slot(self):
         list_courses_by_slot = {}
-        for k, v in courses.items():
-            # 2 hours/timeslot
-            contact_hours = int(v["Contact hours"] / 2)
-            # 7 weeks/ period
-            needed_slot_per_week = self.calculate_need_slot_per_week(k)
-            if needed_slot_per_week == 1:
-                list_courses_by_slot[k] = v
+        list_courses_per_programme = {}
+        for k, v in self.default_courses.items():
+            programme_name = v["Programme"]
+            list_courses_per_programme.setdefault(programme_name, []).append(k)
+
+        for k, v in list_courses_per_programme.items():
+            if len(v) > 5:
+                for c in v:
+                    contact_hours = int(self.default_courses[c]["Contact hours"] / 2)
+                    needed_slot_per_week = math.ceil(contact_hours / 7)
+                    course = self.default_courses[c].copy()
+                    course1 = self.default_courses[c].copy()
+                    full_contact_hours = course["Contact hours"]
+                    if needed_slot_per_week > 2:
+                        for t in range(math.ceil(needed_slot_per_week / 2)):
+                            if t != math.ceil(needed_slot_per_week / 2) - 1:
+                                full_contact_hours -= 12
+                                course["Contact hours"] = 12
+                                list_courses_by_slot[c + "_" + str(t)] = course
+                            else:
+                                course1["Contact hours"] = full_contact_hours
+                                list_courses_by_slot[c + "_" + str(t)] = course1
+                    else:
+                        list_courses_by_slot[c] = self.default_courses[c]
             else:
-                for t in range(needed_slot_per_week):
-                    list_courses_by_slot[k + "_" + str(t)] = v
+                for c in v:
+                    list_courses_by_slot[c] = self.default_courses[c]
         return list_courses_by_slot
